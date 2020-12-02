cdc645f9b11068f25bc71ae77d7472f249793d0d
ak0chkin <kochkin@protonmail.com>
2020-12-03 02:09:28 +0500

    loader + функциональные компоненты

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -1,34 +1,23 @@
 import React from 'react';
 import './index.css';
 
-class Filter extends React.Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    componentDidMount() {
-
-    }
-
-    render() {
-        const filter = this.props.filter.map((item, idx) => (
-            <div key={idx} className="checkbox-wrap">
-                <div className="checkbox-wrap__item">
-                    <input id={`cb${idx}`} type="checkbox" value={item.steps}
-                           checked={item.checked} onChange={this.props.onChange}/><label
-                    htmlFor={`cb${idx}`}>{item.text}</label>
-                </div>
-
+function Filter(props) {
+    const filter = props.filter.map((item, idx) => (
+        <div key={idx} className="checkbox-wrap">
+            <div className="checkbox-wrap__item">
+                <input id={`cb${idx}`} type="checkbox" value={item.steps}
+                       checked={item.checked} onChange={props.onChange}/><label
+                htmlFor={`cb${idx}`}>{item.text}</label>
             </div>
-        ));
-        return (
-            <div className="filter">
-                <div className="filter__title">Количество пересадок</div>
-                {filter}
-            </div>
-        );
-    }
+
+        </div>
+    ));
+    return (
+        <div className="filter">
+            <div className="filter__title">Количество пересадок</div>
+            {filter}
+        </div>
+    );
 }
 
 export default Filter;
--- src/components/Sorting/index.js
+++ src/components/Sorting/index.js
@@ -1,28 +1,21 @@
 import React from 'react';
 import './index.css';
 
-class Sorting extends React.Component {
-    constructor(props) {
-        super(props);
-
-    }
-
-    render() {
-        return (
-            <div className="sorting">
-                <ul className="sorting__tabs">
-                    <li id="price" className={`sorting__tab ${this.props.sorting == 'price' ? 'is-active' : ''}`}
-                        onClick={this.props.handleSorting}>
-                        Самый дешевый
-                    </li>
-                    <li id="speed" className={`sorting__tab ${this.props.sorting == 'speed' ? 'is-active' : ''}`}
-                        onClick={this.props.handleSorting}>
-                        Самый быстрый
-                    </li>
-                </ul>
-            </div>
-        );
-    }
+function Sorting(props) {
+    return (
+        <div className="sorting">
+            <ul className="sorting__tabs">
+                <li id="price" className={`sorting__tab ${props.sorting === 'price' ? 'is-active' : ''}`}
+                    onClick={props.handleSorting}>
+                    Самый дешевый
+                </li>
+                <li id="speed" className={`sorting__tab ${props.sorting === 'speed' ? 'is-active' : ''}`}
+                    onClick={props.handleSorting}>
+                    Самый быстрый
+                </li>
+            </ul>
+        </div>
+    );
 }
 
 export default Sorting;
